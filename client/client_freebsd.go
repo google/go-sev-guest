7e24fda4c0965d889dc2a918972bda041722f725
Yage Hu <yagehu@google.com>
2026-05-27 16:58:52 +0000

    Support FreeBSD build
    This commit makes go-sev-guest compile on FreeBSD.
    FreeBSD does not have guest support for SEV yet, so the implementation
    just contains stubs that return errors.

---
+++ client/client_freebsd.go
@@ -0,0 +1,80 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+
+	spb "github.com/google/go-sev-guest/proto/sevsnp"
+)
+
+// DefaultSevGuestDevicePath is the platform's usual device path to the SEV guest.
+const DefaultSevGuestDevicePath = "unknown"
+
+// FreeBSDDevice implements the Device interface with Linux ioctls.
+type FreeBSDDevice struct{}
+
+// Open is not supported on FreeBSD.
+func (*FreeBSDDevice) Open(_ string) error {
+	return fmt.Errorf("FreeBSD is unsupported")
+}
+
+// OpenDevice fails on FreeBSD.
+func OpenDevice() (*FreeBSDDevice, error) {
+	return nil, fmt.Errorf("FreeBSD is unsupported")
+}
+
+// Close is not supported on FreeBSD.
+func (*FreeBSDDevice) Close() error {
+	return fmt.Errorf("FreeBSD is unsupported")
+}
+
+// Ioctl is not supported on FreeBSD.
+func (*FreeBSDDevice) Ioctl(_ uintptr, _ any) (uintptr, error) {
+	return 0, fmt.Errorf("FreeBSD is unsupported")
+}
+
+// Product is not supported on FreeBSD.
+func (*FreeBSDDevice) Product() *spb.SevProduct {
+	return &spb.SevProduct{}
+}
+
+// FreeBSDQuoteProvider implements the QuoteProvider interface with Linux's configfs-tsm.
+type FreeBSDQuoteProvider struct{}
+
+// IsSupported checks if the quote provider is supported.
+func (*FreeBSDQuoteProvider) IsSupported() bool {
+	return false
+}
+
+// GetRawQuote returns byte format attestation plus certificate table via ConfigFS.
+func (*FreeBSDQuoteProvider) GetRawQuote(reportData [64]byte) ([]byte, error) {
+	return nil, fmt.Errorf("FreeBSD is unsupported")
+}
+
+// GetRawQuoteAtLevel returns byte format attestation plus certificate table via ConfigFS.
+func (*FreeBSDQuoteProvider) GetRawQuoteAtLevel(reportData [64]byte, level uint) ([]byte, error) {
+	return nil, fmt.Errorf("FreeBSD is unsupported")
+}
+
+// GetQuoteProvider returns a supported SEV-SNP QuoteProvider.
+func GetQuoteProvider() (QuoteProvider, error) {
+	return nil, fmt.Errorf("FreeBSD is unsupported")
+}
+
+// GetLeveledQuoteProvider returns a supported SEV-SNP LeveledQuoteProvider.
+func GetLeveledQuoteProvider() (LeveledQuoteProvider, error) {
+	return nil, fmt.Errorf("FreeBSD is unsupported")
+}
